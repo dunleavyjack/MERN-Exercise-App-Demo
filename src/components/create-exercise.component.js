0684e5e8205113b5dcbda2d7a0b1776b7939c256
dunleavyjack <dunleavyjack@gmail.com>
2021-03-29 07:27:21 +0900

    Updated

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class CreateExercise extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>You are on the Create Exercise component</p>
+            </div>
+        )
+    }
+}
